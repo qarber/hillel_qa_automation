a5f8a7f1ec35f6569b1f31acc43379a839e4b792
qarber <r.berezovchuk@gmail.com>
2023-10-26 13:49:15 +0300

    lesson_4 done

---
+++ lesson_4_js_basics.js
@@ -0,0 +1,77 @@
+/* 1. Написати функцію, яка у випадку якщо типи двох аргументів string поверне їх зконкатенований вираз,
+якщо типи двох аргументів number - поверне їх помножене значення
+та у будь якому випадку не співпадіння поверне текст There are no type matches for operation */
+
+function typeChecker(arg1, arg2) {
+// ваш код
+if (typeof (arg1,arg2) === 'string') {
+    return (arg1 + arg2);
+}
+
+else if (typeof arg1 === 'number' && typeof arg2 === 'number') {
+    return (arg1 * arg2);
+}
+
+else {
+    return 'There are no type matches for operation';
+}
+}
+
+// Приклад роботи функції:
+console.log(typeChecker(2, 3)); // виведе 6
+console.log(typeChecker("My", "text")); // виведе Mytext
+console.log(typeChecker(true, 5)); // виведе There are no type matches for operation
+
+/* 2. Написати функцію, яка, використовуючи тернарний оператор, виводить у консоль Yes, якщо число
+позитивне, та No якщо воно негативне */
+function isNegative(value) {
+// ваш код
+(value < 0) ? console.log('Yes') : console.log('No');
+}
+
+// Приклад роботи функції:
+isNegative(-1); // виведе у консоль "Yes"
+isNegative(1); // виведе у консоль "No"
+
+/* 3. Написати функцію, яка повертає true якщо перший аргумент ділиться на другий без остачі, 
+або якщо ні - повертає false */
+function divider(num1, num2) {
+// ваш код
+let result = num1 % num2;
+if (result === 0) {
+    return true
+}
+else {
+    return false
+}
+// ще як варіант намагався заюзати тернарний (Number.isInteger(result)) ? console.log('true') : console.log('false');
+// при умові якщо просто ділити num1 / num2 БЕЗ остачі
+// але в такому кейсі чомусь виводить undefined крім true і false (нерозібрався чому) 
+}
+
+// Приклад роботи функції:
+console.log(divider(4, 2)); // виведе у консоль true
+console.log(divider(3, 2)); // виведе у консоль false
+
+// /* 4. Написати функцію, яка рекурсивно виводить в консоль аргумент та збільшує його на 2 якщо аргумент не
+// перевищує за значенням число 10 */
+function recursiveAddition(value) {
+// ваш код
+let decresedValue = value + 2;
+if (value <= 10) {
+    console.log(value);
+}
+
+if (decresedValue <= 10) {
+    recursiveAddition(decresedValue);
+    }
+    else {
+        console.log();
+
+    }
+}
+
+// Приклад роботи функції:
+recursiveAddition(5); // виведе в консоль по черзі 5, 7, 9
+recursiveAddition(2); // виведе в консоль по черзі 2, 4, 6, 8, 10
+recursiveAddition(11); // не виведе нічого
